efa84d9e6c518056f3aa2923aaeccab4c6c23e2a
Victor Westerlund <victor.vesterlund@gmail.com>
2024-04-06 22:48:59 +0000

    fix: add GitHub links to frameworks on about page (#6)

--- pages/about.php
+++ pages/about.php
@@ -5,12 +5,19 @@
 </section>
 <hr aria-hidden="true">
 <section class="about">
-	<p>I&ZeroWidthSpace;'m a full-stack web developer from Sweden, currently working as IT-Lead at <a href="https://icellate.com">iCellate&nbsp;Medical</a> in Solna, Stockholm - a biopharma start-up developing precision oncology. I develop and maintain <a href="https://docs.vlw.one/vegvisir">my own web framework</a> and use it to build web apps and websites - including this one.</p>
+	<p>I&ZeroWidthSpace;'m a full-stack web developer from Sweden, currently working as IT-Lead at <a href="https://icellate.com">iCellate&nbsp;Medical</a> in Solna, Stockholm - a biopharma start-up developing precision oncology. I develop and maintain <a href="https://github.com/VictorWesterlund/vegvisir">my own web framework</a> and use it to build web apps and websites - including this one.</p>
 	<p>The &lt;programming/markup/command&gt;-languages I currently use the most are (in a mostly accurate decending order): PHP, JavaScript, CSS, MySQL, Python, SQLite, Bash, and [raw] HTML.</p>
 </section>
 <section class="about">
 	<h2>This website</h2>
 	<p>This site and all of its components are 100% Free Software; licensed under the GNU GPLv3. It's built on top of my own <a href="">Vegvisir</a> (web) and <a href="">Reflect</a> (API) framework. There are no cookies or trackers on this site and analytics <strong>only</strong> consist of basic access and error logs; and from which IP address.</p>
+</section>
+<section class="about">
+	<h2>Projects</h2>
+	<p>These are my top projects I'm working on right now:</p>
+	<p>* <a href="https://github.com/VictorWesterlund/vegvisir">Vegvisir</a>: A web framework written in PHP, for PHP developers.</p>
+	<p>* <a href="https://github.com/VictorWesterlund/reflect">Reflect</a>: An API framework also written in PHP, for PHP developers.</p>
+	<p>See more on my <a href="work" vv="about" vv-call="navigate">works page</a>. And even more including smaller projects on my <a href="https://github.com/VictorWesterlund">GitHub</a>.</p>
 </section>
 <section class="about">
 	<h2>Personal</h2>
